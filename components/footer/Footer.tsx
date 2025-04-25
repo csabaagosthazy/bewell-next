668bc26a722430d4b3f6a26a1678f7c252e61ce8
Csaba Agosthazy <40959918+csabaagosthazy@users.noreply.github.com>
2025-04-25 08:09:48 +0200

    Footer update

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -4,33 +4,111 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
-import RestoreIcon from '@mui/icons-material/Restore'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import LocationOnIcon from '@mui/icons-material/LocationOn'
+import PhoneIcon from '@mui/icons-material/Phone'
+import EmailIcon from '@mui/icons-material/Email'
+import TextSnippetIcon from '@mui/icons-material/TextSnippet'
+
+import { NameSpace } from '@/lib/translations'
 
 import './Footer.css'
 import CustomText from '../CustomText/CustomText'
-
+import { useTranslation } from '@/providers/TranslationProvider'
+import Link from 'next/link'
+import { Stack } from '@mui/material'
+const NAMESPACE: NameSpace = 'footer'
 export default function Footer() {
-  const [value, setValue] = React.useState(0)
-
+  const { t } = useTranslation()
   return (
     <Box>
-      <BottomNavigation
-        className='footer'
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue)
-        }}
-      >
-        <CustomText tName='navigation' tKey='home' textType='nav-button' />
-        <BottomNavigationAction label='Recents' icon={<RestoreIcon />} />
-        <BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
-        <BottomNavigationAction label='Nearby' icon={<LocationOnIcon />} />
+      <BottomNavigation className='footer'>
+        <Box className='footer-icons'>
+          <Stack
+            className='footer-stack'
+            spacing={{ xs: 1, sm: 1 }}
+            direction='row'
+          >
+            <PhoneIcon className='footer-icon' />
+            <CustomText
+              tName={NAMESPACE}
+              tKey={'phone'}
+              textType='footer-label'
+            />
+          </Stack>
+          <Stack
+            className='footer-stack'
+            spacing={{ xs: 1, sm: 1 }}
+            direction='row'
+          >
+            <EmailIcon className='footer-icon' />
+            <CustomText
+              tName={NAMESPACE}
+              tKey={'email'}
+              textType='footer-label'
+            />
+          </Stack>
+          <Stack
+            className='footer-stack'
+            spacing={{ xs: 1, sm: 1 }}
+            direction='row'
+          >
+            <TextSnippetIcon className='footer-icon' />
+            <Link href={'/imprint'}>
+              <CustomText
+                tName={NAMESPACE}
+                tKey={'imprint'}
+                textType='footer-label'
+              />
+            </Link>
+          </Stack>
+        </Box>
+        <Box className='footer-copyright'>
+          <CustomText
+            tName={NAMESPACE}
+            tKey={'copyright'}
+            textType='footer-text'
+          />
+        </Box>
       </BottomNavigation>
     </Box>
   )
 }
 
 // <Box className='footer' sx={{ width: 500 }}>
+
+{
+  /* <BottomNavigationAction
+showLabel
+label={
+  <CustomText
+    tName={NAMESPACE}
+    tKey={'phone'}
+    textType='footer-label'
+  />
+}
+icon={<PhoneIcon className='footer-icon' />}
+/>
+<BottomNavigationAction
+showLabel
+label={
+  <CustomText
+    tName={NAMESPACE}
+    tKey={'email'}
+    textType='footer-label'
+  />
+}
+icon={<EmailIcon className='footer-icon' />}
+/>
+<BottomNavigationAction
+showLabel
+label={
+  <Link href={'/imprint'}>
+    <CustomText
+      tName={NAMESPACE}
+      tKey={'imprint'}
+      textType='footer-label'
+    />
+  </Link>
+}
+icon={<TextSnippetIcon className='footer-icon' />}
+/> */
+}
